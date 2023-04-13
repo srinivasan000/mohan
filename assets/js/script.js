1ce281be6919fd4126e96aa79ea4d6db985de8b1
srinivasan000 <srinivasans2425s@gmail.com>
2023-04-13 21:30:10 +0530

    msg

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+var loader = document.querySelector(".loading");
+var container = document.querySelector(".container");
+addEventListener("load", () => {
+  loader.style.display = "none";
+  container.style.display = "block";
+});
